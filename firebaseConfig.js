405569158ecc28c497c41f70cf92334ae4f5a30b
NyoTaimori <ondiguidominique14@gmail.com>
2026-03-06 03:20:38 +0100

    F1

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6VwNowvbtM_vWJG7IszoGP6CItZgcDd0",
+  authDomain: "tapatan-go.firebaseapp.com",
+  projectId: "tapatan-go",
+  storageBucket: "tapatan-go.firebasestorage.app",
+  messagingSenderId: "349484264207",
+  appId: "1:349484264207:web:84a8f5268d5e3934062c7a",
+  measurementId: "G-N22F4RH0LG"
+};
